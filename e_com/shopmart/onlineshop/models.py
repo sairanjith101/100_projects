3344a3bfe7a2802a5bc804654a3b1b8d5e6610e1
sairanjith101 <sairanjith101@gmail.com>
2024-05-14 22:26:33 +0530

    update

--- e_com/shopmart/onlineshop/models.py
+++ e_com/shopmart/onlineshop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
+import os
 
 # Create your models here.
+
+def getFileName(request, filename):
+    now_time = datetime.datetime.now().strftime("%Y%m%d%H:%M:%S")
+    new_filename = "%s%s"%(now_time, filename)
+    return os.path.join('uploads/', new_filename)
+
+
+class Catagory(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False)
+    image = models.ImageField(upload_to=getFileName, null=True, blank=True)
