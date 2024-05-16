6a7cedd7094fe48757aa46df6615a2777db00008
sairanjith101 <sairanjith101@gmail.com>
2024-05-16 22:28:45 +0530

    update

--- e_com/shopmart/onlineshop/admin.py
+++ e_com/shopmart/onlineshop/admin.py
@@ -3,5 +3,11 @@ from .models import *
 
 # Register your models here.
 
+# class CatagoryAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'image', 'description']
+
+# admin.site.register(Catagory, CatagoryAdmin)
+
+
 admin.site.register(Catagory)
 admin.site.register(Product)
