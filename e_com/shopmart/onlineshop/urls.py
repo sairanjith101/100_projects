819d4c73d485800c00378c73b390c91b6c453447
sairanjith101 <sairanjith101@gmail.com>
2024-06-03 22:30:26 +0530

    add

--- e_com/shopmart/onlineshop/urls.py
+++ e_com/shopmart/onlineshop/urls.py
@@ -4,5 +4,6 @@ from onlineshop import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('collections/', views.collections, name='collections')
 ]
--- e_com/shopmart/onlineshop/views.py
+++ e_com/shopmart/onlineshop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -6,3 +7,7 @@ def home(request):
 
 def register(request):
     return render(request, 'onlineshop/register.html')
+
+def collections(request):
+    catagory = Catagory.objects.filter(status=0)
+    return render(request, 'onlineshop/collections.html', {"catagory":catagory})
