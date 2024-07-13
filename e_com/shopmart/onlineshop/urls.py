c8a4858e123f62bc19bfa1a6b51b4b6dca7197ff
sairanjith101 <sairanjith101@gmail.com>
2024-07-13 20:34:19 +0530

    add

--- e_com/shopmart/onlineshop/urls.py
+++ e_com/shopmart/onlineshop/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('collections/', views.collections, name='collections'),
-    path('collections/<str:name>', views.collectionsview, name='collections'),
-    path('collections/<str:cname>/<str:pname>', views.product_details, name='product_details'),
+    path('collections/<str:name>/', views.collectionsview, name='collections'),
+    path('collections/<str:cname>/<str:pname>/', views.product_details, name='product_details'),
 ]
