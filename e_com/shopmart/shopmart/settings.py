f6b8ea9edafcda870da39c6b786ed559c6019b99
sairanjith101 <sairanjith101@gmail.com>
2024-07-19 11:20:31 +0530

    updated

--- e_com/shopmart/shopmart/settings.py
+++ e_com/shopmart/shopmart/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/uploads/'
-# MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
