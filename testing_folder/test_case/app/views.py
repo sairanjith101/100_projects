ac4c1a81c0fb8b3b9391b3bd162a8ac23b6171b8
sairanjith101 <sairanjith101@gmail.com>
2024-08-01 10:42:55 +0530

    test_case created

---
+++ testing_folder/test_case/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'app/index.html')
+
+def register(request):
+    return render(request, 'app/register.html')
