7b8e28302c81cdcf140fd035e17f8a8c53b814e5
sairanjith101 <139247459+sairanjith101@users.noreply.github.com>
2024-08-07 10:39:01 +0530

    Delete testing_folder/test_case directory

--- testing_folder/test_case/app/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from app import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('register', views.register, name='register'),
-]
--- testing_folder/test_case/app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def home(request):
-    return render(request, 'app/index.html')
-
-def register(request):
-    return render(request, 'app/register.html')
