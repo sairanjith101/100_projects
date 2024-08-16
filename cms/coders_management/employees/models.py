17fee70c59421b340418ed0258f429a734af06b7
sairanjith101 <sairanjith101@gmail.com>
2024-08-16 15:05:47 +0530

    employee models created

--- cms/coders_management/employees/models.py
+++ cms/coders_management/employees/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=10, unique=True)
+    extension_number = models.CharField(max_length=5, blank=True)
+    tl_name = models.CharField(max_length=100, blank=True)
+    photo = models.ImageField(upload_to='photos/', blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} - {self.employee_id}'
