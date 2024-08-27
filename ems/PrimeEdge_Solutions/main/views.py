786f53509afffbfdfef83e366ee5604c6afa3d69
sairanjith101 <sairanjith101@gmail.com>
2024-08-27 11:19:30 +0530

    profile page and employees page completed

--- ems/PrimeEdge_Solutions/main/views.py
+++ ems/PrimeEdge_Solutions/main/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .forms import LoginForm
 from .models import Employee, Department, Attendance
+from django.db.models import Q
+from django.db import models
+
 
 
 # Create your views here.
@@ -30,12 +33,46 @@ def login_view(request):
 
 @login_required
 def profile_info(request):
-    return render(request, 'main/profile.html')
+    try:
+        # Get the employee profile associated with the logged-in user
+        employee = Employee.objects.get(user=request.user)
+    except Employee.DoesNotExist:
+        # Handle the case where the employee profile does not exist
+        employee = None
+    return render(request, 'main/profile.html', {'employee':employee})
 
 @login_required
 def employees_page(request):
-    employees = Employee.objects.all()
+    # Get search criteria from the query parameters
+    id_query = request.GET.get('id', '')
+    name_query = request.GET.get('name', '')
+    email_query = request.GET.get('email', '')
+    phone_query = request.GET.get('phone', '')
+    position_query = request.GET.get('position', '')
+
+    # Initialize employees variable
+    employees = None
+    
+    # If there is any search criteria, filter the employees
+    if id_query or name_query or email_query or phone_query or position_query:
+        employees = Employee.objects.all()
+        
+        if id_query:
+            employees = employees.filter(employee_id__icontains=id_query)
+        if name_query:
+            employees = employees.filter(
+                Q(first_name__icontains=name_query) |
+                Q(last_name__icontains=name_query)
+            )
+        if email_query:
+            employees = employees.filter(email__icontains=email_query)
+        if phone_query:
+            employees = employees.filter(phone_number__icontains=phone_query)
+        if position_query:
+            employees = employees.filter(position__icontains=position_query)
+
     return render(request, 'main/employees.html', {'employees': employees})
+
 
 @login_required
 def help_page(request):
